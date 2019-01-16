f6252ebd20862fa0f739381f3eee562263bff1f2
Xuelian Li <lixuelian8807@gmail.com>
2019-01-15 17:52:39 -0800

    spec address book

---
+++ spec/AddressBookSpec.js
@@ -0,0 +1,37 @@
+describe('Address Book', function(){
+  var addressBook,
+      newContact;
+
+  beforeEach(function(){
+    addressBook = new AddressBook(),
+    newContact = new Contact();
+  });
+
+  it('should be able to add a contact', function(){
+    addressBook.addContact(newContact);
+
+    expect(addressBook.getContact(0)).toBe(newContact);
+  });
+
+  it('should be able to delete a contact', function(){
+    addressBook.addContact(newContact);
+    addressBook.deleteContact(0);
+
+    expect(addressBook.getContact(0)).not.toBeDefined();
+  });
+});
+
+describe('Async Address Book',function(){
+  var addressBook = new AddressBook();
+
+  beforeEach(function(done){
+    addressBook.getInitialContacts(function(){
+      done();
+    });
+  });
+
+  it('should get inital contacts', function(done){
+    expect(addressBook.initialComplete).toBe(true);
+    done();
+  });
+});
